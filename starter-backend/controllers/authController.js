415871d61885200293edee1887b3cb6610973e5a
DLee <dlee1004@cs.washington.edu>
2025-02-18 17:20:58 -0800

    barebones database and auth

--- starter-backend/controllers/authController.js
+++ starter-backend/controllers/authController.js
@@ -1,16 +1,29 @@
 // handles user signup with authentication
 const { createUserWithEmailAndPassword } = require("firebase/auth")
-const { auth } = require("../config/firebase.js")
+const { auth, database } = require("../config/firebase.js") 
 
 
-// createUserWithEmailAndPassword(auth, email, password)
-//   .then((userCredential) => {
-//     // Signed up 
-//     const user = userCredential.user;
-//     // ...
-//   })
-//   .catch((error) => {
-//     const errorCode = error.code;
-//     const errorMessage = error.message;
-//     // ..
-//   });
+const signup = async function (req, res) {
+    try {
+        const { email, firstName, lastName, username, password} = req.body;
+    
+        const userCredentials = await createUserWithEmailAndPassword(auth, email, password)
+
+        await database.collection("users").doc(userCredentials.user.uid).set({
+            email: email, 
+            firstName: firstName,
+            lastName: lastName,
+            username: username
+        })
+        
+    } catch (error) {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log(errorCode)
+        console.log(errorMessage)
+    }
+
+
+}
+
+  module.exports = { signup };
